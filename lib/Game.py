d596fc59294e0ea3cd9fb68efec9e14c6b100425
scottkc97 <scott97kc@gmail.com>
2020-04-10 11:33:58 -0400

    Revert "Revert "Revert "move tags"""
    This reverts commit 8da05269ef47ad2246cc3e4c8aa4899bc7315cc3.

--- lib/Game.py
+++ lib/Game.py
@@ -8,65 +8,64 @@ Classes that are useful for a simulation of the game
 @author: liwenouyang
 """
 
-
+from scipy import special
 import networkx as nx 
 import random 
+import numpy as np
 
 class Game:
     """A class representing a single state of the game we try to simulate"""
-    def __init__(self,G):
+    def __init__(self, numAgent, Gini, para, distr = 'lognormal'):
         """Constructor
         
-        Args: G, a preprocessed graph with node attributes set as player class defined below
-              M, payoff matrix  class definded below
+        Args: 
+        numAgent: the number of agents 
+        Gini: the initial gini coefficient
+        para: parameter depending on distribution chosen for initial wealth distribution. 
+                If lognormal, para = mu. If expontential, para = lambda. If uniform, para= a.
+        distr: lognormal, exp, uni
         """
-        self.network = G
-        self.players = nx.get_node_attributes(G,'player')
+        assert distr in ['lognormal','exp','uni'], "invalid distribution"
+        assert numAgent >= 1
+        wealth = []
+        if distr == 'lognormal':
+            sig = special.erfinv(Gini) * 2
+            for i in range(numAgent):
+                wealth.append(int(random.lognormvariate(para, sig)))
+        if distr == 'exp':
+            for i in range(numAgent):
+                wealth.append(int(random.expovariate(para)))
+        if distr == 'uni':
+            b = (3*Gini+1)/(1 - 3*Gini)
+            for i in range(numAgent):
+                wealth.append(int(random.uniform(para, b)))
+        g = nx.complete_graph(numAgent)
+        for i in range(numAgent):
+            g.nodes[i]['player'] = Player(i,a = wealth[i], s = 'coop')
+        self.network = g
+        self.players = nx.get_node_attributes(g, 'player')
         
-    def threshold(self, f):
-        """calculate threshold for breaking ties based on the current state base on a function f"""
-        ass = [] # not intended, you know the hardest thing in programming is naming your variables :)
+    def wealth_distr(self):
+        """return the wealth of agents as a list"""
+        wealth = []
         for p in self.players.values():
-            ass.append(p.asset)
-        return f(ass)
-    
-    def succ(self,f = lambda x: sum(x)/len(x)):  #successor 
-         """Given a current game state and a function f (defaulted to be mean) as a threshold, return the game state of next round """
-                        
-        for e in list(self.network.edges):
-            a = self.players[e[0]]
-            b = self.players[e[1]]
-            M  = Matrix(a,b)
-            P = M.payoff()
-            a.asset += P[a.id]
-            b.asset += P[b.id]
+            wealth.append(p.asset)
+        return wealth 
         
-        def breakTie(a,b):
-            """Given a,b as players, returns true if a want to break tie with b or the other way around"""
-            return not(a.asset >= self.threshold(f) and b.asset >= self.threshold(f))
-        
-        
-        for e in list(self.network.edges):
-            a = self.players[e[0]]
-            b = self.players[e[1]]
-            if breakTie(a, b):
-                self.network.remove_edge(e[0],e[1])
-        
-        return self
-    
-    def toImage(self):
-        """plot the graph that represents the current state"""
-        pass
-    
-    def assets(self):
-        """return a dictionary that contains the assets every agent possess in the current state"""
-        pass
-    
-    def distr(self):
-        """plot the distribution of assets of the current state"""
-        pass
-
-    def gini(self)
+    def naive(self, a, b, w):
+        """A naive way to calculate the probability of breaking ties given two players's gain a, b and average global gain:
+            If both above average, the probability of breaking tie is low: 0.1
+            If both below average, a high probability of breaking tie: 0.9
+            Otherwise 50/50 chance 
+            """
+        if a >= w and b >= w:
+            return 0.1
+        elif a < w and b < w:
+            return 0.9
+        else:
+            return 0.5
+            
+    def gini(self):
         """Returns the difference-based Gini based on Bowles/Carlin
 
         delta is sum of abs differences between agents
@@ -74,28 +73,71 @@ class Game:
         n is total number of agents
         G is Gini
         """
-        pass # for now
+        pass
+        #n = (numAgents * (numAgents - 1)) / 2 # num edges
+        #delta = 0 
+        #ybar = mean(Game.wealth_distr[]) # find avg of wealth
+        #for j in range(n - 1):
+        #    for (j + 1) in range(n):
+        #        delta += abs(Game.wealth_distr[i] - Game.wealth_distr[j]) # obv make this correspond to wealth attribute
+        
+        #G = (delta / (n * (n - 1))) * ( 1 / ybar )
+        
+        #return G     
     
-        # n = number of edges
-        # delta = 0
-        # ybar = mean(y[:]) # find avg of wealth
-        # for j in range(n - 1):
-        #     for (j + 1) in range(n):
-        #         delta += abs(y[i] - y[j]) # obv make this correspond to wealth attribute
-        # 
-        # G = (delta / (n * (n - 1))) * ( 1 / ybar )
-        #
-        #return G 
+    def succ(self,f = naive):  #successor  
+        """Given a current game state and a function f , return the game state of next round 
+        Args:
+            f(a,b,w): a function that takes in three numbers and returns a number in [0,1] as the probability of breaking tie.
+            where a is the gain of player A, and b is the gain of player B. w is the average gain of this round. 
+        """
+        totalgain = 0
+        
+        for e in list(self.network.edges):
+            a = self.players[e[0]]
+            b = self.players[e[1]]
+            M  = Matrix(a,b)
+            P = M.payoff()
+            a.asset += P[a.id]
+            a.gain += P[a.id]
+            b.asset += P[b.id]
+            b.gain += P[b.id]
+        
+        for p in self.players.values():
+            totalgain += p.gain
+        
+        avg = totalgain/ len(self.players)
+        for e in list(self.network.edges):
+            a = self.players[e[0]]
+            b = self.players[e[1]]
+            pob = f(a.gain,b.gain,avg)
+            choice = np.random.choice(['b','m'],p = [pob, 1-pob])
+            if choice == 'b':
+                self.network.remove_edge(e[0], e[1])
+       
+        for n in self.players.values():
+            n.gain = 0
+            # Reset gain for next round 
+        
+        return self
+    
+    def toImage(self):
+        """plot the graph that represents the current state"""
+        
+        pass
+    
+    
+
+
 
 class Player:
-    """A player class that represents every single player with their strategy as str, 
-    current assets as int, and the actions taken against certain opponent as a dict with opponents' id as keys 
-    and list of actions taken as values"""
+    """A player class that represents every single player with their strategy as str, current assets as int, and the actions taken as a list"""
     def __init__(self,ID,a = 0, s = 'random'):
         self.id = ID
         self.acts = {}
         self.asset = a
         self.strat = s
+        self.gain = 0
     #A note: if define constructor as init(self,ID, A=[],a=0,s = 'r'), p1 and p2 will be updated simultaneously when calling takeAct. 
     def takeAct(self,opponent,C = ('E', 'L')):
         """Given an opponent and two choices as C, get the action based on the strategy, and update Actions taken"""
@@ -136,25 +178,8 @@ class Matrix:
             return {p1:6, p2: 0}
         if a1 == self.c2 and a2 == self.c2:
             return {p1:2, p2: 2}
+  
     
- 
 
-"""
-testNet = nx.Graph()
-for i in [1,2,3]:
-    testNet.add_node(i, player = Player(i))
-testNet.add_edges_from([(1,2),(2,3),(1,3)])
-g = Game(testNet)
-g1 = g.succ().succ() #A test case for two rounds on a complete graph with 3 nodes 
-"""
-
-
-"""
-testNet = nx.Graph()
-for i in range(30):
-    testNet.add_node(i, player = Player(i))
-E = [(random.randrange(0, 30), random.randrange(0, 30)) for i in range(40)]
-testNet.add_edges_from(E)
-g = Game(testNet)
-g1 = g.succ().succ() #A test case for two rounds on a much larger and denser graph randomly generated.
-"""
+        
+        
