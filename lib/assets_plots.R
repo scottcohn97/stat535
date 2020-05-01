2127e38a938d0ece23b80638d26fec3eb6a1c09e
scottkc97 <scott97kc@gmail.com>
2020-05-01 15:12:41 -0400

    process / plot new sims

--- lib/assets_plots.R
+++ lib/assets_plots.R
@@ -10,11 +10,28 @@ library('ggpubr')
 # run once at start
 # font_import(pattern = "lmroman10-regular-webfont") 
 
-g01p <- read.csv("g01p.csv")
-g03p <- read.csv("g03p.csv")
-g07p <- read.csv("g07p.csv")
-g09p <- read.csv("g09p.csv")
+# Import
+g01p <- read_csv("output/g01p.csv")
+g03p <- read_csv("output/g03p.csv")
+g07p <- read_csv("output/g07p.csv")
+g09p <- read_csv("output/g09p.csv")
 
+g01_hc1p <- read_csv("output/g01_hc1p.csv")
+g03_hc1p <- read_csv("output/g03_hc1p.csv")
+g07_hc1p <- read_csv("output/g07_hc1p.csv")
+g09_hc1p <- read_csv("output/g09_hc1p.csv")
+
+g01_hc2p <- read_csv("output/g01_hc2p.csv")
+g03_hc2p <- read_csv("output/g03_hc2p.csv")
+g07_hc2p <- read_csv("output/g07_hc2p.csv")
+g09_hc2p <- read_csv("output/g09_hc2p.csv")
+
+g01_hc3p <- read_csv("output/g01_hc3p.csv")
+g03_hc3p <- read_csv("output/g03_hc3p.csv")
+g07_hc3p <- read_csv("output/g07_hc3p.csv")
+g09_hc3p <- read_csv("output/g09_hc3p.csv")
+
+# Func
 asset.plot <- function(df){
     p <- df %>%
         group_by(strategies) %>%
@@ -36,20 +53,72 @@ asset.plot <- function(df){
     return(p)
 }
 
+# Plot
 p01 <- asset.plot(g01p)
 p03 <- asset.plot(g03p)
 p07 <- asset.plot(g07p)
 p09 <- asset.plot(g09p)
 
+p01_hc1 <- asset.plot(g01_hc1p)
+p03_hc1 <- asset.plot(g03_hc1p)
+p07_hc1 <- asset.plot(g07_hc1p)
+p09_hc1 <- asset.plot(g09_hc1p)
+
+p01_hc2 <- asset.plot(g01_hc2p)
+p03_hc2 <- asset.plot(g03_hc2p)
+p07_hc2 <- asset.plot(g07_hc2p)
+p09_hc2 <- asset.plot(g09_hc2p)
+
+p01_hc3 <- asset.plot(g01_hc3p)
+p03_hc3 <- asset.plot(g03_hc3p)
+p07_hc3 <- asset.plot(g07_hc3p)
+p09_hc3 <- asset.plot(g09_hc3p)
+
 #Save plot to PDF
-ggsave(p01, filename = "p01.pdf", 
+ggsave(p01, filename = "figs/p01.pdf", 
        width = 9, height = 7, units = "in")
 
-ggsave(p03, filename = "p03.pdf", 
+ggsave(p03, filename = "figs/p03.pdf", 
        width = 9, height = 7, units = "in")
 
-ggsave(p07, filename = "p07.pdf", 
+ggsave(p07, filename = "figs/p07.pdf", 
        width = 9, height = 7, units = "in")
 
-ggsave(p09, filename = "p09.pdf", 
+ggsave(p09, filename = "figs/p09.pdf", 
+       width = 9, height = 7, units = "in")
+#
+ggsave(p01_hc1, filename = "figs/p01_hc1.pdf", 
+       width = 9, height = 7, units = "in")
+
+ggsave(p03_hc1, filename = "figs/p03_hc1.pdf", 
+       width = 9, height = 7, units = "in")
+
+ggsave(p07_hc1, filename = "figs/p07_hc1.pdf", 
+       width = 9, height = 7, units = "in")
+
+ggsave(p09_hc1, filename = "figs/p09_hc1.pdf", 
+       width = 9, height = 7, units = "in")
+#
+ggsave(p01_hc2, filename = "figs/p01_hc2.pdf", 
+       width = 9, height = 7, units = "in")
+
+ggsave(p03_hc2, filename = "figs/p03_hc2.pdf", 
+       width = 9, height = 7, units = "in")
+
+ggsave(p07_hc2, filename = "figs/p07_hc2.pdf", 
+       width = 9, height = 7, units = "in")
+
+ggsave(p09_hc2, filename = "figs/p09_hc2.pdf", 
+       width = 9, height = 7, units = "in")
+#
+ggsave(p01_hc3, filename = "figs/p01_hc3.pdf", 
+       width = 9, height = 7, units = "in")
+
+ggsave(p03_hc3, filename = "figs/p03_hc3.pdf", 
+       width = 9, height = 7, units = "in")
+
+ggsave(p07_hc3, filename = "figs/p07_hc3.pdf", 
+       width = 9, height = 7, units = "in")
+
+ggsave(p09_hc3, filename = "figs/p09_hc3.pdf", 
        width = 9, height = 7, units = "in")
--- lib/process_output.R
+++ lib/process_output.R
@@ -6,10 +6,28 @@
 library('tidyverse')
 
 # Import 
-gini_01 <- read.csv("g1.csv")
-gini_03 <- read.csv("g3.csv")
-gini_07 <- read.csv("g7.csv")
-gini_09 <- read.csv("g9.csv")
+
+# standard treatment --- uniform strat distro
+gini_01 <- read_csv("output/g1.csv")
+gini_03 <- read_csv("output/g3.csv")
+gini_07 <- read_csv("output/g7.csv")
+gini_09 <- read_csv("output/g9.csv")
+
+# high coop 1 - 3
+gini_01_hc1 <- read_csv("output/g1_hc1.csv")
+gini_03_hc1 <- read_csv("output/g3_hc1.csv")
+gini_07_hc1 <- read_csv("output/g7_hc1.csv")
+gini_09_hc1 <- read_csv("output/g9_hc1.csv")
+
+gini_01_hc2 <- read_csv("output/g1_hc2.csv")
+gini_03_hc2 <- read_csv("output/g3_hc2.csv")
+gini_07_hc2 <- read_csv("output/g7_hc2.csv")
+gini_09_hc2 <- read_csv("output/g9_hc2.csv")
+
+gini_01_hc3 <- read_csv("output/g1_hc3.csv")
+gini_03_hc3 <- read_csv("output/g3_hc3.csv")
+gini_07_hc3 <- read_csv("output/g7_hc3.csv")
+gini_09_hc3 <- read_csv("output/g9_hc3.csv")
 
 # Process func
 process.csv <- function(df){
@@ -25,13 +43,49 @@ process.csv <- function(df){
 
 # Process and write to csv
 g01p <- process.csv(gini_01)
-write.csv(g01p, file = 'g01p.csv', row.names = TRUE)
+write_csv(g01p, path = 'output/g01p.csv')
 
 g03p <- process.csv(gini_03)
-write.csv(g03p, file = 'g03p.csv', row.names = TRUE)
+write_csv(g03p, path = 'output/g03p.csv')
 
 g07p <- process.csv(gini_07)
-write.csv(g07p, file = 'g07p.csv', row.names = TRUE)
+write_csv(g07p, path = 'output/g07p.csv')
 
 g09p <- process.csv(gini_09)
-write.csv(g09p, file = 'g09p.csv', row.names = TRUE)
+write_csv(g09p, path = 'output/g09p.csv')
+# 
+g01_hc1p <- process.csv(gini_01_hc1)
+write_csv(g01_hc1p, path = 'output/g01_hc1p.csv')
+
+g03_hc1p <- process.csv(gini_03_hc1)
+write_csv(g03_hc1p, path = 'output/g03_hc1p.csv')
+
+g07_hc1p <- process.csv(gini_07_hc1)
+write_csv(g07_hc1p, path = 'output/g07_hc1p.csv')
+
+g09_hc1p <- process.csv(gini_09_hc1)
+write_csv(g09_hc1p, path = 'output/g09_hc1p.csv')
+#
+g01_hc2p <- process.csv(gini_01_hc2)
+write_csv(g01_hc2p, path = 'output/g01_hc2p.csv')
+
+g03_hc2p <- process.csv(gini_03_hc2)
+write_csv(g03_hc2p, path = 'output/g03_hc2p.csv')
+
+g07_hc2p <- process.csv(gini_07_hc2)
+write_csv(g07_hc2p, path = 'output/g07_hc2p.csv')
+
+g09_hc2p <- process.csv(gini_09_hc2)
+write_csv(g09_hc2p, path = 'output/g09_hc2p.csv')
+#
+g01_hc3p <- process.csv(gini_01_hc3)
+write_csv(g01_hc3p, path = 'output/g01_hc3p.csv')
+
+g03_hc3p <- process.csv(gini_03_hc3)
+write_csv(g03_hc3p, path = 'output/g03_hc3p.csv')
+
+g07_hc3p <- process.csv(gini_07_hc3)
+write_csv(g07_hc3p, path = 'output/g07_hc3p.csv')
+
+g09_hc3p <- process.csv(gini_09_hc3)
+write_csv(g09_hc3p, path = 'output/g09_hc3p.csv')
